691ea18c7b2dee8c81763ab6cace0ab6cc012c31
Erick Kariuki <erickbinkariuki@gmail.com>
2026-02-20 10:21:03 +0300

    Deploy

--- Expense/settings.py
+++ Expense/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-uq(0kc_+8_oex4xze30)0p8r-cld_#mj_=ho82ysffacjr1429'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["*.onrender.com"]
 
 
 # Application definition
